893c3d253272319c64251fda7c6a69057c2fd87c
Tosuu Digital <76971156+tosuufx@users.noreply.github.com>
2021-04-11 14:33:59 +0200

    Added main file

---
+++ main.py
@@ -0,0 +1,26 @@
+import os, signal
+import subprocess, sys
+
+path = os.path.expanduser('~') + "\\.spicetify\\themes\\*" # Themes path
+choice = input("[*] Select theme :") #Asking for the theme
+choice.strip(' ')
+configPath = os.path.expanduser('~') + "\\.spicetify\\config.ini" # Config file path
+configRead = open(configPath, "r")
+
+lines = configRead.readlines() # Reading config file
+lines[28] = "current_theme           = "+choice # Replacing theme
+
+configWrite = open(configPath, "w")
+configWrite.writelines(lines) # Overwriting
+configWrite.close()
+
+# Updating Spotify
+p = subprocess.Popen(["powershell.exe",
+              "spicetify update"],
+              stdout=sys.stdout)
+p.communicate()
+
+# Restarting Spotify
+spotify = os.path.expanduser("~")+'\AppData\Roaming\Spotify\Spotify.exe'
+os.system("TASKKILL /F /IM Spotify.exe")
+os.execl(spotify, os.path.abspath(__file__))
